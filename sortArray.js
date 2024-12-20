d8f89346b0f99d40782dae99410d04e6aea59343
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-12-20 11:52:04 +0800

    sortArray.js

--- sortArray.js
+++ sortArray.js
@@ -1,1 +1,19 @@
+// sortArray.js
+function sortArr() {
+  let numbers = [24, 65, 21, 5, 9, 32, 42, 80, 57];
+  let names = ["Zenvo", "Erica", "Jordie", "Alicia", "Redon"];
 
+  // Merge the arrays
+  let mergedArr = numbers.concat(names);
+  console.log("Merged array:", mergedArray);
+
+  // Sort numbers in reverse numerical order
+  numbers.sort((a, b) => b - a); 
+  console.log("Sorted numbers (descending):", numbers);
+
+  // Sort names alphabetically
+  names.sort();
+  console.log("Sorted names (ascending):", names);
+}
+
+sortArrays();
